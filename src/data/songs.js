406285a84bdc108cd3033bd3d532421016c65d1d
Mathesh <99591069+matheshyogeswaran@users.noreply.github.com>
2024-12-21 20:26:09 +0530

    data songs added

---
+++ src/data/songs.js
@@ -0,0 +1,38 @@
+export const recommendedSongs = [
+    {
+        url: 'https://eslvjmuxvenmevluurws.supabase.co/storage/v1/object/public/ncs/Royalty.mp3?t=2024-12-21T14%3A33%3A14.327Z',
+        title: 'Royalty',
+        artist: 'Egzod, Maestro Chives, Neoni',
+        artwork: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/936/325x325/royalty-1619082030-xBgqGZWLw9.jpg',
+    },
+    {
+        url: 'https://eslvjmuxvenmevluurws.supabase.co/storage/v1/object/public/ncs/YourPoison.mp3?t=2024-12-21T14%3A34%3A50.636Z',
+        title: 'Royalty',
+        artist: 'ROY KNOX',
+        artwork: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/786/325x325/your-poison-1728561655-YBgf7fx1Bb.jpg',
+    },
+    {
+        url: 'https://eslvjmuxvenmevluurws.supabase.co/storage/v1/object/public/ncs/YourPoison.mp3?t=2024-12-21T14%3A34%3A50.636Z',
+        title: 'Desperate',
+        artist: 'NEFFEX, Tokyo Machine',
+        artwork: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/757/325x325/desperate-1727222452-FZRAmrmXyX.jpg',
+    },
+    {
+        url: 'https://eslvjmuxvenmevluurws.supabase.co/storage/v1/object/public/ncs/backstreet.mp3?t=2024-12-21T14%3A50%3A41.211Z',
+        title: 'Backstreet Boy',
+        artist: 'Netrum, Halvorsen',
+        artwork: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/821/325x325/backstreet-boy-1734656457-1mj02zkpxh.jpg',
+    },
+    {
+        url: 'https://eslvjmuxvenmevluurws.supabase.co/storage/v1/object/public/ncs/Ripple.mp3?t=2024-12-21T14%3A52%3A23.106Z',
+        title: 'Gimme',
+        artist: 'Ripple',
+        artwork: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/820/325x325/gimme-1734570059-e67zlEsw2Y.png',
+    },
+    {
+        url: 'https://eslvjmuxvenmevluurws.supabase.co/storage/v1/object/public/ncs/afterTheEnd.mp3?t=2024-12-21T14%3A54%3A26.409Z',
+        title: 'After the End',
+        artist: 'Rex Hooligan',
+        artwork: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/819/325x325/after-the-end-1734397255-9Lh9UTTj82.jpg',
+    },
+]
---
+++ src/data/songsWithCategory.js
@@ -0,0 +1,16 @@
+import { recommendedSongs } from "./songs";
+
+export const songsWithCategory = [
+    {
+        title: 'Recommended for you',
+        songs: recommendedSongs
+    },
+    {
+        title: 'New Relese',
+        songs: recommendedSongs
+    },
+    {
+        title: 'NCS Songs',
+        songs: recommendedSongs
+    }
+]
